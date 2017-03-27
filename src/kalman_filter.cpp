4f2eeab159cca4121cce09228750fe0a7533aae6
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2017-03-27 10:58:39 -0400

    updates for complete model

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,6 @@
 #include "kalman_filter.h"
-
+#include "tools.h"
+#include <iostream>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -18,22 +19,41 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new state
+  x_ = x_ +  (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size,x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  //convert to polar coordinates
+  VectorXd Hx = VectorXd(3);
+  Hx << sqrt(pow(x_[0],2) + pow(x_[1],2)), atan(x_[1]/x_[0]), (x_[0]* x_[2] + x_[1]*x_[3])/ sqrt(pow(x_[0],2) + pow(x_[1],2));
+  VectorXd y = z - Hx;
+  //H_ is set to Jacobian in FusionEKF
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  //new estimate
+  x_ = x_ +  (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size,x_size);
+  P_ = (I - K * H_) * P_;
 }
